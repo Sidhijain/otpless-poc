6730f9d08b9d7643b8d2d7fd8c09c4e456c89d9e
Sidhijain <sidhijain482@gmail.com>
2026-07-15 11:36:24 +0530

    callback changes

--- js/app.js
+++ js/app.js
@@ -11,15 +11,58 @@ function updateStatus(title, data = "") {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const callback = (event) => {
-    console.log(event);
+  console.log("Inisde evenet listner");
+  // const callback = (event) => {
+  //   console.log("event",event);
+  //   updateStatus(event.responseType || "Callback", event);
+  //   if (event.responseType === "ONETAP") {
+  //     setTimeout(() => {
+  //       location.href = "success.html";
+  //     }, 1500);
+  //   }
+  // };
+  const callback = async (eventCallback) => {
+    alert("EVENT = " + eventCallback.responseType);
 
-    updateStatus(event.responseType || "Callback", event);
+    console.log(eventCallback);
 
-    if (event.responseType === "ONETAP") {
-      setTimeout(() => {
+    updateStatus(eventCallback.responseType, eventCallback);
+
+    const EVENTS_MAP = {
+      OTP_AUTO_READ: async () => {
+        const otp = eventCallback.response?.otp;
+
+        alert("OTP AUTO READ : " + otp);
+
+        const phone = document.getElementById("mobileNumber").value.trim();
+
+        const result = await OTPlessSignin.verify({
+          channel: "PHONE",
+          phone,
+          countryCode: "+91",
+          otp,
+        });
+
+        console.log(result);
+      },
+
+      ONETAP() {
+        alert("SUCCESS");
+
         location.href = "success.html";
-      }, 1500);
+      },
+
+      FAILED() {
+        alert(JSON.stringify(eventCallback.response));
+      },
+
+      FALLBACK_TRIGGERED() {
+        alert("FALLBACK");
+      },
+    };
+
+    if (eventCallback.responseType in EVENTS_MAP) {
+      EVENTS_MAP[eventCallback.responseType]();
     }
   };
 
@@ -30,24 +73,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function initiateAuth() {
   const phone = document.getElementById("mobileNumber").value.trim();
-
   if (!phone) {
     alert("Enter Mobile Number");
-
     return;
   }
-
   updateStatus("Initiating Authentication...");
-
   try {
     const result = await OTPlessSignin.initiate({
       channel: "PHONE",
-
       phone,
-
       countryCode: "+91",
     });
-
     updateStatus("INITIATE", result);
   } catch (error) {
     console.error(error);
